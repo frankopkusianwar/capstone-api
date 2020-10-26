f9fbf3be6918c4f4933f054d864cd58d36a7c739
frankopkusianwar <okirorfrank3@gmail.com>
2020-10-26 21:12:02 +0300

    Add bcrypt gem

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,4 @@
 class Reservation < ApplicationRecord
   belongs_to :room
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  # encrypt password
+  has_secure_password
+
+  # Model associations
+  has_many :reservations, foreign_key: :reserved_by
+  # Validations
+  validates_presence_of :name, :password_digest
 end
