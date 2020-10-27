9cda239ceed850312a2a900a51ff30d88d1d0dbe
frankopkusianwar <okirorfrank3@gmail.com>
2020-10-27 12:02:42 +0300

    Update authorization restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  # skip_before_action :authorize_request, only: :create
+  skip_before_action :authorize_request, only: :create
   # POST /signup
   # return authenticated token upon signup
   def create
--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -36,7 +36,7 @@ module ExceptionHandler
     rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
 
     rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
+      render json: { message: e.message }, status: :not_found
     end
   end
 
@@ -44,11 +44,11 @@ module ExceptionHandler
 
   # JSON response with message; Status code 422 - unprocessable entity
   def four_twenty_two(e)
-    json_response({ message: e.message }, :unprocessable_entity)
+    render json: { message: e.message }, status: :unprocessable_entity
   end
 
   # JSON response with message; Status code 401 - Unauthorized
   def unauthorized_request(e)
-    json_response({ message: e.message }, :unauthorized)
+    render json: { message: e.message }, Status: :unauthorized
   end
 end
