4606602c9108f9717eba8f26d8f0f26cfe50ef21
frankopkusianwar <okirorfrank3@gmail.com>
2020-10-27 17:21:45 +0300

    Fix testing bug

--- spec/controllers/reservations_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ReservationsController, type: :controller do
-
-end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 # Test suite for the Reservation model
 RSpec.describe Reservation, type: :model do
   # Association test
-  it { should belong_to(:Room) }
-  it { should belong_to(:User) }
+  it { should belong_to(:room) }
+  it { should belong_to(:user) }
 
   # Validation test
   it { should validate_presence_of(:date) }
