c6c84113b056adf0d21fababcaac77d389279c26
frankopkusianwar <okirorfrank3@gmail.com>
2020-10-27 08:42:04 +0300

    Fix login bug

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,7 +5,7 @@ class AuthenticationController < ApplicationController
   def authenticate
     auth_token =
       AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
-    json_response(auth_token: auth_token)
+    render json: { auth_token: auth_token }
   end
 
   private
