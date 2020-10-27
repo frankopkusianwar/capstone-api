a0b6cbf22befd024ac055ae51fac1022d3894a85
frankopkusianwar <okirorfrank3@gmail.com>
2020-10-27 18:04:31 +0300

    Add get rooms test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,9 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
 
@@ -66,4 +69,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include RequestSpecHelper, type: :request
 end
