791aa13febc4d1b5298d6fb136c7eb86842e3170
frankopkusianwar <okirorfrank3@gmail.com>
2020-10-28 11:30:51 +0300

    Add get one room endpoint

--- app/controllers/v1/rooms_controller.rb
+++ app/controllers/v1/rooms_controller.rb
@@ -1,11 +1,16 @@
 module V1
   class RoomsController < ApplicationController
 
-    skip_before_action :authorize_request, only: :index
+    skip_before_action :authorize_request, only: [:index, :show]
 
     def index
       rooms = Room.all.paginate(page: params[:page], per_page: 5)
       render json: rooms
+    end
+
+    def show
+      room = Room.find(params[:id])
+      render json: room
     end
   end
 end
