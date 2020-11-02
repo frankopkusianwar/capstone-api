9b077df2527d2b668bed40f9ce496ff97ad3d8fe
frankopkusianwar <okirorfrank3@gmail.com>
2020-11-02 17:22:52 +0300

    Fix cors error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,10 @@
 class ApplicationController < ActionController::API
   # include Response
   # include ExceptionHandler
-  protect_from_forgery
-  before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers
+
   # called before every action on controllers
   before_action :authorize_request
   attr_reader :current_user
-  
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
-
-  def cors_preflight_check
-    if request.method == "OPTIONS"
-      headers['Access-Control-Allow-Origin'] = 'http://localhost'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :content_type => 'text/plain'
-    end
-  end
 
   private
 
