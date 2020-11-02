4d24c41481b82a6a1389d8c01391b60bd758f0c4
frankopkusianwar <okirorfrank3@gmail.com>
2020-11-03 00:22:01 +0300

    Add created status code to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
     response = { message: Message.account_created, auth_token: auth_token }
-    render json: response
+    render json: response, status: :created
   end
 
   private
