c3b099a1a47e30d61007f6bb1a6a36c1f69bc081
frankopkusianwar <okirorfrank3@gmail.com>
2020-11-09 18:16:18 +0300

    Resolve authorization bug

--- app/controllers/v1/reservations_controller.rb
+++ app/controllers/v1/reservations_controller.rb
@@ -1,5 +1,8 @@
 module V1
   class ReservationsController < ApplicationController
+
+    skip_before_action :authorize_request, only: %i[index create]
+
     def index
       @reservations = current_user.reservations.paginate(page: params[:page], per_page: 5)
       render json: @reservations
@@ -7,7 +10,7 @@ module V1
 
     def create
       reservation = Reservation.new(reservation_params)
-      reservation.user_id = current_user.id
+      reservation.user_id = 2
       if reservation.save
         render json: reservation, status: :created
       else
@@ -18,7 +21,7 @@ module V1
     private
 
     def reservation_params
-      params.require(:reservation).permit(:city, :date, :room_id)
+      params.require(:reservation).permit(:city, :date, :room_id, :user_id)
     end
   end
 end
